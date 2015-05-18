bde6678586ad56a5b36c103323a31e5a141d2250
Rashik <tuladhar.rashik@gmail.com>
2015-05-18 19:41:38 +0545

    updated the portfolio my skills section added

---
+++ mysite/index.php
@@ -0,0 +1,298 @@
+<html>
+    <head>
+        <title>Rashik Tuladhar | A Web Developer From Nepal</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+        <link rel="stylesheet" type="text/css" href="css/mystyle.css">
+        <link rel="icon" href="images/logo.png">
+    </head>
+<body id="home">
+
+
+<!-- Fixed navbar -->
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php"></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About Me</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#myskills">My Skills</a></li>
+                <li><a href="#contact">Contact Me</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
+<!-- Fixed navbar -->
+
+
+<section id="about" data-anchor="about">
+    <div class="container aboutme">
+        <div class="col-md-12">
+            <h1>About Me</h1>
+            <img src="images/bg.jpg" class="pull-left img-responsive" >
+            <p class="text-justify">
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.paragraphs'.<br>
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.paragraphs'
+            </p>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</section>
+
+<section id="portfolio" data-anchor="portfolio">
+    <div class="container myportfolio">
+        <div class="row">
+            <h1>My Works</h1>
+            <div class="col-md-3 col-sm-6">
+                <h2>Heading</h2>
+                <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+                    tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+                    Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                </p>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h2>Heading</h2>
+                <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+                    tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+                    Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                </p>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h2>Heading</h2>
+                <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+                    tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+                    Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                </p>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h2>Heading</h2>
+                <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+                    tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+                    Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                </p>
+            </div>
+
+            <div class="col-md-3 col-sm-6">
+                <h2>Heading</h2>
+                <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+                    tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+                    Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                </p>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h2>Heading</h2>
+                <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+                    tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+                    Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                </p>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h2>Heading</h2>
+                <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+                    tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+                    Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                </p>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h2>Heading</h2>
+                <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+                    tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+                    Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<section id="myskills">
+    <div class="container myskills">
+        <div class="col-md-12">
+            <h1>My Skills</h1>
+
+            <div class="skillbar clearfix " data-percent="20%">
+                <div class="skillbar-title" style="background: #d35400;"><span>HTML5</span></div>
+                <div class="skillbar-bar" style="background: #e67e22;"></div>
+                <div class="skill-bar-percent">20%</div>
+            </div> <!-- End Skill Bar -->
+
+            <div class="skillbar clearfix " data-percent="25%">
+                <div class="skillbar-title" style="background: #2980b9;"><span>CSS3</span></div>
+                <div class="skillbar-bar" style="background: #3498db;"></div>
+                <div class="skill-bar-percent">25%</div>
+            </div> <!-- End Skill Bar -->
+
+            <div class="skillbar clearfix " data-percent="50%">
+                <div class="skillbar-title" style="background: #2c3e50;"><span>jQuery</span></div>
+                <div class="skillbar-bar" style="background: #2c3e50;"></div>
+                <div class="skill-bar-percent">50%</div>
+            </div> <!-- End Skill Bar -->
+
+            <div class="skillbar clearfix " data-percent="40%">
+                <div class="skillbar-title" style="background: #46465e;"><span>PHP</span></div>
+                <div class="skillbar-bar" style="background: #5a68a5;"></div>
+                <div class="skill-bar-percent">40%</div>
+            </div> <!-- End Skill Bar -->
+
+            <div class="skillbar clearfix " data-percent="75%">
+                <div class="skillbar-title" style="background: #333333;"><span>Wordpress</span></div>
+                <div class="skillbar-bar" style="background: #525252;"></div>
+                <div class="skill-bar-percent">75%</div>
+            </div> <!-- End Skill Bar -->
+
+            <div class="skillbar clearfix " data-percent="100%">
+                <div class="skillbar-title" style="background: #27ae60;"><span>SEO</span></div>
+                <div class="skillbar-bar" style="background: #2ecc71;"></div>
+                <div class="skill-bar-percent">100%</div>
+            </div> <!-- End Skill Bar -->
+
+            <div class="skillbar clearfix " data-percent="70%">
+                <div class="skillbar-title" style="background: #124e8c;"><span>Photoshop</span></div>
+                <div class="skillbar-bar" style="background: #4288d0;"></div>
+                <div class="skill-bar-percent">70%</div>
+            </div> <!-- End Skill Bar -->
+        </div>
+    </div>
+</section>
+
+
+
+<section id="contact">
+    <div class="container contact">
+        <div class="row">
+            <h1>Contact Me</h1>
+            <div class="col-md-3 col-sm-6">
+                <h2>Heading</h2>
+                <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+                    tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+                    Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                </p>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h2>Heading</h2>
+                <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+                    tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+                    Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                </p>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h2>Heading</h2>
+                <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+                    tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+                    Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                </p>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h2>Heading</h2>
+                <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+                    tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+                    Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                </p>
+            </div>
+
+            <div class="col-md-3 col-sm-6">
+                <h2>Heading</h2>
+                <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+                    tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+                    Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                </p>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h2>Heading</h2>
+                <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+                    tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+                    Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                </p>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h2>Heading</h2>
+                <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+                    tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+                    Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                </p>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h2>Heading</h2>
+                <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+                    tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+                    Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+<footer>
+    <div class="container">
+        Rashik Tuladhar &copy; 2015, Nepal <i class="fa fa-flag-checkered"></i>
+
+    </div>
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<script type="text/javascript">
+
+//for easy scrolling
+    $("nav ul li a[href^='#']").on('click', function(e) {
+        // prevent default anchor click behavior
+        e.preventDefault();
+        // store hash
+        var hash = this.hash;
+        // animate
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 1000, function(){
+            // when done, add hash to url
+            // (default click behaviour)
+            window.location.hash = hash;
+        });
+    });
+
+
+//animation of skills
+    jQuery(document).ready(function(){
+        jQuery('.skillbar').each(function(){
+            jQuery(this).find('.skillbar-bar').animate({
+                width:jQuery(this).attr('data-percent')
+            },6000);
+        });
+    });
+</script>
+</body>
+</html>
